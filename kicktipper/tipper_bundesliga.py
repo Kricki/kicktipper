5a7156c8b1fe1a8bd79d517c81a2a567ea616c79
Kricki <euphiment@gmx.de>
2019-09-11 22:41:14 +0200

    Add store_data_for_matchday_to_file

--- kicktipper/tipper_bundesliga.py
+++ kicktipper/tipper_bundesliga.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from typing import Union
+from datetime import datetime
+import os
 
 from . import kicktipp_api
 from . import fivethirtyeight
@@ -7,9 +9,10 @@ from . import predictor
 from . import tools
 
 
-
 class TipperBundesliga:
     def __init__(self, kicktipp_group):
+        self._datapath = '../data'
+
         self.kicktipp_group = kicktipp_group
         self.kicktipp_username = None
         self.kicktipp_password = None
@@ -35,8 +38,8 @@ class TipperBundesliga:
             dfc.at[i, 'team2'] = name
         return dfc
 
-    @staticmethod
-    def leaguetable_read(filename='../data/Bundesliga.csv'):
+    def leaguetable_read(self, filename='Bundesliga.csv'):
+        filename = os.path.join(self._datapath, filename)
         return pd.read_csv(filename)
 
     def login(self):
@@ -62,7 +65,8 @@ class TipperBundesliga:
         return df
 
     def projected_scores_for_match(self, team1, team2):
-        match = self.projected_scores[(self.projected_scores['team1']==team1) & (self.projected_scores['team2']==team2)]
+        match = self.projected_scores[(self.projected_scores['team1'] == team1)
+                                      & (self.projected_scores['team2'] == team2)]
         ps1 = match['proj_score1'].values[0]
         ps2 = match['proj_score2'].values[0]
 
@@ -108,7 +112,7 @@ class TipperBundesliga:
         df_ps['prob_draw'] = prob_draw
         return df_ps
 
-    def predict_and_submit_scores_for_matchday(self, matchday: Union[int, list]=None):
+    def predict_and_submit_scores_for_matchday(self, matchday: Union[int, list] = None):
         """
         Predict scores for a matchday and submit these scores the Kicktipp website.
 
@@ -126,3 +130,26 @@ class TipperBundesliga:
             scores = list(map(list, zip(*scores)))  # transpose list of lists
             # see https://stackoverflow.com/questions/6473679/transpose-list-of-lists/6473727
             self._kicktipp_api.submit_predictions(scores=scores, matchday=md)
+
+    def store_data_for_matchday_to_file(self, filename=None, overwrite=False):
+        df = self.kicktipp_matches_read()
+        df_proj_score = self.projected_scores_for_matchday()
+        df_pred_score = self.predicted_scores_for_matchday()
+        df = df.drop('date', 1)
+        df['proj_score1'] = df_proj_score['proj_score1']
+        df['proj_score2'] = df_proj_score['proj_score2']
+        df['pred_score1'] = df_pred_score['pred_score1']
+        df['pred_score2'] = df_pred_score['pred_score2']
+        dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        df['timestamp'] = [dt]*len(df.index)
+
+        if filename is None:
+            md = df['matchday'][0]
+            filename = os.path.join(self._datapath, 'matchday' + str(md) + '.csv')
+        else:
+            filename = os.path.join(self._datapath, filename)
+
+        if not overwrite and os.path.isfile(filename):
+            return
+        else:
+            df.to_csv(filename, index=False)
